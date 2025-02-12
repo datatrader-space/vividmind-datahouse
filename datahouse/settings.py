461a17abc9141c643fe0a31c6dda721a6285c8e9
chtalal-x <talalkhan9009@gmail.com>
2025-02-12 17:53:22 +0500

    updates

--- datahouse/settings.py
+++ datahouse/settings.py
@@ -74,14 +74,24 @@ WSGI_APPLICATION = 'datahouse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+} """
+from decouple import config
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '5432',
+        #'ATOMIC_REQUESTS': True,
+    }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
