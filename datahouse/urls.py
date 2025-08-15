2ef97320632dcbe96fe451f89bbf1e1d3b6592d9
hamza <hamza@datatrader.space>
2025-08-15 18:21:54 +0500

    update statics

--- datahouse/urls.py
+++ datahouse/urls.py
@@ -26,3 +26,9 @@ urlpatterns = [
      path('admin/', admin.site.urls),
     path('datahouse/',include('core.urls'))
 ]
+
+from django.conf.urls.static import static
+from django.conf import settings
+urlpatterns += static(
+    settings.MEDIA_URL,document_root=settings.MEDIA_ROOT
+)
